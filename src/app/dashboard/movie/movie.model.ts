c829da1752ea807a58481195501f5309a278867a
Daniel <dedd1993@gmail.com>
2019-09-12 23:36:09 -0500

    feat(movie): make specific model

--- src/app/dashboard/movie/movie.model.ts
+++ src/app/dashboard/movie/movie.model.ts
@@ -1,14 +1,26 @@
 export interface Movie {
     adult:                  boolean;
     backdrop_path:          string;
-    genre_ids:              number[];
+    belongs_to_collection?: BelongsToCollection;
+    budget?:                number;
+    genre_ids?:             number[];
+    genres?:                Genres[];
+    homepage?:              string;
     id:                     number;
+    imdb_id?:               string; // minlength: 9; maxlength: 9; pattern: ^tt[0-9]{7}
     original_language:      string;
     original_title:         string;
     overview:               string;
     popularity:             number;
     poster_path:            string;
+    production_companies?:  ProductionCompanies;
+    production_countries?:  ProductionCountries;
     release_date:           string;
+    revenue?:               number;
+    runtime?:               number;
+    spoken_languages?:      SpokenLanguages;
+    status?:                string;
+    tagline?:               string;
     title:                  string;
     video:                  boolean;
     vote_average:           number;
@@ -20,4 +32,26 @@ interface BelongsToCollection {
     id:            number
     name:          string;
     poster_path:   string;
+}
+
+interface Genres {
+    id:   number;
+    name: string;
+}
+
+interface ProductionCompanies {
+    id:             number;
+    logo_path:      string;
+    name:           string;
+    origin_country: string;
+}
+
+interface ProductionCountries {
+    iso_3166_1: string;
+    name: string;
+}
+
+interface SpokenLanguages {
+    iso_3166_1: string;
+    name: string;
 }

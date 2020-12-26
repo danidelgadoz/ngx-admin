3c35876b22e9df930dda530f2030b3cde3088ab4
Dandel <dedd1993@gmail.com>
2020-12-26 16:24:53 -0500

    fix(auth): fix redirection for unauthorized views

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -18,7 +18,7 @@ export class AuthGuard implements CanActivate {
     }
 
     // not logged in so redirect to login page with the return url
-    this.router.navigate(['/authentication/login'], { queryParams: { returnUrl: state.url } });
+    this.router.navigate(['/auth/login'], { queryParams: { returnUrl: state.url } });
     return false;
   }
 }

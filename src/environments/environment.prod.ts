e4dd9ab7ead5ec3f5b43f64e3a97f9908af75fad
Dandel <dedd1993@gmail.com>
2020-12-26 15:54:26 -0500

    env: update mockable domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
   backend: {
-    host: 'https://demo3682812.mockable.io',
+    host: 'https://demo0034835.mockable.io',
   },
   oauth: {
-    host: 'https://demo3682812.mockable.io',
+    host: 'https://demo0034835.mockable.io',
     client_id: '2',
     client_secret: 'tsN80QNwTawD3WZSX2uziOFI6HstTEs2bXBqsCyv',
     scope: '*',
